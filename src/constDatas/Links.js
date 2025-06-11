4bc7b106e8221f9271895e494d069280e08f0dc1
abhishek <dhitalabhishek982gmail.com>
2025-06-11 10:33:20 +0545

    commission calculator

--- src/constDatas/Links.js
+++ src/constDatas/Links.js
@@ -14,7 +14,7 @@ export const QuickLinksData = [
   {
     title: "Agent App",
     description: "",
-    link: "",
+    link: "https://forms.zohopublic.com.au/CIHE/form/AgencyRequestApplication1/formperma/1PRnx2lGlk2LDSJGOpTbJcnB4ZrfkPNwaOUHLkyB93Q",
     icon: "fi fi-tr-journal-alt",
   },
 
@@ -25,19 +25,19 @@ export const MarketingKitData = [
   {
     title: "Photo Pack",
     description: "",
-    link: "https://churchill.nsw.edu.au/assets/apply-at-churchill.pdf",
+    link: "hhttps://churchillinstituteofhighere-my.sharepoint.com/:f:/g/personal/marketing_churchill_nsw_edu_au/EhmCry00Uv5AknYooXoFT9EB5HBw3GbKzpEQ-WAulmgLOw?e=97cTg8",
     icon: "fi fi-tr-images",
   },
   {
     title: "Churchill Institute's logo",
     description: "",
-    link: "https://forms.zohopublic.com.au/CIHE/form/Refund/formperma/0InT099rx3HO6tfccMlx2R5tZQmV9MOOekjGBUjMx1g",
+    link: "https://churchillinstituteofhighere-my.sharepoint.com/:f:/g/personal/marketing_churchill_nsw_edu_au/Ev6yVaj0fHxNn_Qp9pU8SDYBcX5vQ8jc7ZorDLcHF-dCDQ?e=GhhmdX",
     icon: "fi fi-tr-customize-computer",
   },
   {
     title: "Promotional videos",
     description: "",
-    link: "",
+    link: "https://churchillinstituteofhighere-my.sharepoint.com/:f:/g/personal/marketing_churchill_nsw_edu_au/Ev_AXXRgw-hAhrK63RnPbyoBzlbw3loTvvJxSdo7bpTAAQ?e=pUmYyk",
     icon: "fi fi-tr-films",
   },
 ];
